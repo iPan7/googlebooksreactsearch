d7a228bda5bfc8ca917a48271c5adc5f2f39124f
Ian Panlilio <ipanlilio7@gmail.com>
2020-12-02 20:06:00 -0800

    added react component skel skeletons and back end folders

---
+++ client/src/pages/search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class search extends React.Component {
+    render() {
+        return (
+            <div>Hello World!</div>
+        );
+    }
+}
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://username:password!@googlebooksreactsearchd.exviq.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
