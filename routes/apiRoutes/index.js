0e3f33b95b7719eaf8962429c7a67e73c5758dd1
Ian Panlilio <ipanlilio7@gmail.com>
2020-12-03 16:15:36 -0800

    change routes to match readme reqs

--- routes/apiRoutes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const savedbooksRoutes = require('./savedbooksRoutes')
-
-router.use('/savedbooks', savedbooksRoutes);
-
-module.exports = router;
--- routes/apiRoutes/savedbooksRoutes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', function (req, res) {
-    res.send('Birds home page')
-  })
